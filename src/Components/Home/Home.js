876ebe9678d15d2a62c3064c7bf93ad777ac3f38
Vivek Raj <VivekRaj27082005@gmail.com>
2023-09-01 22:54:19 +0530

    Added Responsiveness to Home Page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,10 @@
 import React, { useEffect} from 'react';
 import './Home.css';
 
-function Home() {
+export default function Home() {
     
     useEffect(() => {
         const H = window.innerHeight;
-        const h1 = 4400 + 3.1 * H;
         window.onbeforeunload = () => {
             window.scrollTo(0, 0);
         };
@@ -26,10 +25,10 @@ function Home() {
         const handleScroll = () => {
             const scroll = window.scrollY;
 
-            if (scroll < 650) {
-                const s1 = 1 - scroll / 1000;
-                const s2 = 1 - scroll / 1500;
-                const s3 = 1 - scroll / 550;
+            if (scroll < 0.5*H) {
+                const s1 = 1 - scroll / (H);
+                const s2 = 1 - scroll / (1.25*H);
+                const s3 = 1 - scroll / (0.35*H);
 
                 L1.forEach(l => {
                     l.style.transform = `scale(${s1})`;
@@ -44,15 +43,15 @@ function Home() {
                 waw.style.opacity = 1;
             }
 
-            if (scroll < 650) {
+            if (scroll < 0.5*H) {
                 span.style.height = '100%';
             }
 
-            if (scroll > 650) {
+            if (scroll > 0.5*H) {
                 span.style.height = '0%';
             }
 
-            if (scroll > 650 && scroll < 1400) {
+            if (scroll > 0.5*H && scroll < 2*H) {
                 L3.forEach(l => {
                     l.classList.add('active');
                 });
@@ -62,42 +61,39 @@ function Home() {
                 });
             }
 
-            if (scroll < 1050) {
+            if (scroll < H) {
                 circle.style.backgroundColor = 'rgb(23, 0, 54)';
+                circle.style.transform = 'scale(3)';
             }
 
-            if (scroll > 2000) {
+            if (scroll > 2.5*H) {
                 waw_cont.style.display = 'none';
             } else {
                 waw_cont.style.display = 'flex';
             }
 
-            if (scroll < 1050) {
-                circle.style.transform = 'scale(3)';
-            }
-
-            if (scroll > 1050 && scroll < 3800) {
-                const s4 = 3 - (scroll - 1050) / 875;
-                const r = 23 + (scroll - 1050) * 0.2;
-                const g = 0 + (scroll - 1050) * 0.2;
-                const b = 54 + (scroll - 1050) * 0.2;
+            if (scroll > 2*H && scroll < 7*H) {
+                const s4 = 3 - (scroll - 2*H) / H;
+                const r = 23 + (scroll - 2*H) * 0.2;
+                const g = 0 + (scroll - 2*H) * 0.2;
+                const b = 54 + (scroll - 2*H) * 0.2;
                 circle.style.transform = s4 < 0 ? 'scale(0)' : `scale(${s4})`;
                 circle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
                 ot.style.opacity = 1;
             }
 
-            if (3800 < scroll) {
+            if (7*H < scroll) {
                 circle.style.transform = 'scale(0)';
             }
-            if (scroll < 3800) {
+            if (scroll < 7*H) {
                 unit.style.top = '100vh';
             }
 
-            if (scroll > 3800 && scroll < h1) {
+            if (scroll > 7*H && scroll < 14.5*H) {
                 ot.style.opacity = 0;
-                const top = (1 - 2 * (scroll - 3800) / (1.5 * H)) * 100;
+                const top = (1 - 2 * (scroll - 7*H) / (3 * H)) * 100;
                 unit.style.top = `${top}vh`;
-                const i = Math.floor((scroll - 3800) / (1.5 * H));
+                const i = Math.floor((scroll - 7*H) / (3 * H));
                 tt[i].classList.remove('deactive');
                 tt[i].classList.add('active');
                 if (i > 0) {
@@ -115,12 +111,12 @@ function Home() {
                 });
             }
 
-            if (h1 < scroll) {
+            if (14.5*H < scroll) {
                 unit.style.top = '-300vh';
             }
-            if (h1 - 600 < scroll) {
-                const j = Math.floor((scroll + 400 - h1) / H);
-                const top = (1 - ((scroll + 400 - h1) % H) / H) * 100;
+            if (14*H < scroll) {
+                const j = Math.floor((scroll - 14*H) / (2*H));
+                const top = (1 - ((scroll - 14*H) % (2*H)) / (2*H)) * 100;
                 if (-1 < j && j < 9) {
                     event_unit[j].style.top = `${top}vh`;
                     event_unit[j].style.bottom = `-${top}vh`;
@@ -136,13 +132,13 @@ function Home() {
                     element.style.bottom = '-100vh';
                 }
             }
-            if (scroll < h1 - 600) {
+            if (scroll < 14*H) {
                 event_unit.forEach(eu => {
                     eu.style.top = '100vh';
                     eu.style.bottom = '-100vh';
                 });
             }
-            if (10 * H + h1 - 1000 < scroll) {
+            if (32*H < scroll) {
                 event_unit.forEach(eu => {
                     eu.style.top = '0vh';
                     eu.style.bottom = '0vh';
@@ -157,12 +153,12 @@ function Home() {
         };
     }, []);
     return (
-        <div className='body-1'>
+        <div className="home-body">
             <div className="container">
                 <div className="circle"></div>
             </div>
             <div className="center" style={{ zIndex: 1 }}>
-                <h1 id="aml">AMALTHEA</h1>
+                <h1 id="aml" className="oswald">AMALTHEA</h1>
             </div>
             <img className="aml-bg-1 L1" src="./Images/AML-BG-1.png" alt="" />
             <img className="aml-bg-2 L1" src="./Images/AML-BG-2.png" alt="" />
@@ -175,25 +171,25 @@ function Home() {
             <img className="drone L3" src="./Images/Drone.png" alt="" />
             <img className="robo L3" src="./Images/Robo.png" alt="" />
             <div id="waw-container">
-                <h2 id="waw">WHO ARE WE?</h2>
+                <h2 id="waw" className="oswald">WHO ARE WE?</h2>
                 <div className="waw-text">
                     <span className="waw-span"></span>
-                    <p>
+                    <p className="montserrat">
                         Amalthea is India's first student-run Annual Technical Summit conducted by IIT Gandhinagar. Since its inception in 2010, Amalthea has been pushing boundaries and setting high standards. With a plethora of exciting events, conclaves and a symposium coupled with a tech expo, we are back offline and ready to go beyond the impossible and blow away your minds. <br /><br />
                         Amalthea believes in fostering innovation and creative thinking by providing a forum for people to exchange their ideas and thoughts. We aim to serve as a bridge between industry and academia in order to connect and discuss pivotal technological prospects. Amalthea aspires to encourage young minds to ponder upon current issues and seek innovative and feasible solutions.
                     </p>
                 </div>
             </div>
-            <div id="ot" className="center" style={{ zIndex: -2 }}>
+            <div id="ot" className="center oswald" style={{ zIndex: -2 }}>
                 <h2>OUR THEME</h2>
             </div>
-            <div className="title center" style={{ zIndex: -3 }}>
+            <div className="title center oswald" style={{ zIndex: -3 }}>
                 <h2>CONCALVE & SYMPOSIUM</h2>
             </div>
-            <div className="title center" style={{ zIndex: -3 }}>
+            <div className="title center oswald" style={{ zIndex: -3 }}>
                 <h2>TECH EXPO</h2>
             </div>
-            <div className="title center" style={{ zIndex: -3 }}>
+            <div className="title center oswald" style={{ zIndex: -3 }}>
                 <h2>THE LOCAL EVENTS</h2>
             </div>
             <div id="unit" style={{ zIndex: -4 }}>
@@ -230,77 +226,116 @@ function Home() {
             <div className="event-unit">
                 <div className="event">
                     <div className="details">
-                        <h2 className="event-name">ROBOWARS</h2>
+                        <h2 className="event-name oswald">ROBOWARS</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
                     </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
+                    
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
                 </div>
             </div>
             <div className="event-unit">
                 <div className="event">
                     <div className="details">
-                        <h2 className="event-name">DRA</h2>
+                        <h2 className="event-name oswald">DRA</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
                     </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
+                </div>
+            </div>
+            <div className="event-unit">
+                <div className="event" style={{backgroundImage: "url(/Images/Drift.png)"}}>
+                    <div className="details">
+                        <h2 className="event-name oswald">DRIFT RACING</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
+                    </div>
+
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
+                </div>
+            </div>
+            <div className="event-unit">
+                <div className="event" style={{backgroundImage: "url(/Images/Brainwiz.png)"}}>
+                    <div className="details">
+                        <h2 className="event-name oswald">BRAINWIZ</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
+                    </div>
+
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
+                </div>
+            </div>
+            <div className="event-unit">
+                <div className="event" style={{backgroundImage: "url(/Images/Game-Jam.png)"}}>
+                    <div className="details">
+                        <h2 className="event-name oswald">GAMEJAM</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
+                    </div>
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
                 </div>
             </div>
             <div className="event-unit">
                 <div className="event">
                     <div className="details">
-                        <h2 className="event-name">DRIFT</h2>
+                        <h2 className="event-name oswald">STOCKRUSH</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
                     </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
                 </div>
             </div>
             <div className="event-unit">
                 <div className="event">
                     <div className="details">
-                        <h2 className="event-name">BRAINWIZ</h2>
+                        <h2 className="event-name oswald">INNOVATION NEXUS</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
                     </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
+                </div>
+            </div>
+            <div className="event-unit">
+                <div className="event" style={{backgroundImage: "url(/Images/Icon.png)"}}>
+                    <div className="details">
+                        <h2 className="event-name oswald">ICON</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
+                    </div>
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
                 </div>
             </div>
             <div className="event-unit">
                 <div className="event">
                     <div className="details">
-                        <h2 className="event-name">GAMEJAM</h2>
+                        <h2 className="event-name oswald">DECODE</h2>
+                        <div className='actions-even-name'>
+                        <h5 className='apply_rule montserrat'>Register </h5>
+                        <h5 className='apply_rule montserrat'>Rule Book </h5>
+                        </div>
                     </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
-                </div>
-            </div>
-            <div className="event-unit">
-                <div className="event">
-                    <div className="details">
-                        <h2 className="event-name">STOCKRUSH</h2>
-                    </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
-                </div>
-            </div>
-            <div className="event-unit">
-                <div className="event">
-                    <div className="details">
-                        <h2 className="event-name">INNOVATION NEXUS</h2>
-                    </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
-                </div>
-            </div>
-            <div className="event-unit">
-                <div className="event">
-                    <div className="details">
-                        <h2 className="event-name">ICON</h2>
-                    </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
-                </div>
-            </div>
-            <div className="event-unit">
-                <div className="event">
-                    <div className="details">
-                        <h2 className="event-name">DECODE</h2>
-                    </div>
-                    <h3 className="date">xx-Nov <br /> 2023</h3>
+                    
+                    <h3 className="date oswald">xx-Nov <br /> 2023</h3>
                 </div>
             </div>
         </div>
     );
 }
 
-export default Home;
